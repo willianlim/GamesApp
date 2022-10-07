616c246cbb2fbcf2e40c3e51cf20013ceef44fbe
Willian-code <willianrosendodelima@outlook.com>
2022-10-07 10:17:50 -0300

    settings to connect to local database

---
+++ src/main/java/com/spring/gamesapp/model/Game.java
@@ -0,0 +1,74 @@
+package com.spring.gamesapp.model;
+
+import java.util.Objects;
+
+public class Game {
+
+    private Long id;
+    private String titulo;
+    private String produtora;
+    private boolean zerado;
+    private int nota;
+    private String console;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getProdutora() {
+        return produtora;
+    }
+
+    public void setProdutora(String produtora) {
+        this.produtora = produtora;
+    }
+
+    public boolean isZerado() {
+        return zerado;
+    }
+
+    public void setZerado(boolean zerado) {
+        this.zerado = zerado;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public String getConsole() {
+        return console;
+    }
+
+    public void setConsole(String console) {
+        this.console = console;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return zerado == game.zerado && nota == game.nota && Objects.equals(id, game.id) && Objects.equals(titulo, game.titulo) && Objects.equals(produtora, game.produtora) && Objects.equals(console, game.console);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titulo, produtora, zerado, nota, console);
+    }
+}
