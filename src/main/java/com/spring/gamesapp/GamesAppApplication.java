5df553efc2c3b0d9cc5dd0525966243d8a770ed9
Willian-code <willianrosendodelima@outlook.com>
2022-10-07 09:36:55 -0300

    basic project structure

---
+++ src/main/java/com/spring/gamesapp/GamesAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gamesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesAppApplication.class, args);
+	}
+
+}
