f006f7f32bda47a047b66624cda2bb77469345b2
Willian-code <willianrosendodelima@outlook.com>
2022-10-07 12:11:02 -0300

    creating methods to get all games and only one

--- src/main/java/com/spring/gamesapp/service/GameService.java
+++ src/main/java/com/spring/gamesapp/service/GameService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/")
 public class GameService {
@@ -16,6 +18,16 @@ public class GameService {
     @GetMapping
     public String testar() {
         return "Olá mundo";
+    }
+
+    @GetMapping("/api/games")
+    public List<Game> listar() {
+        return gameRepository.findAll();
+    }
+
+    @GetMapping("/api/games/{id}")
+    public Game listarJogoPorId(@PathVariable Long id) {
+        return gameRepository.findById(id).get();
     }
 
     @PostMapping("/api/games")
